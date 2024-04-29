c8b34f34d1c39956bf77d98bf3d607d1b71af4ba
Mahdi Amereh <mahdieightt@gmail.com>
2024-04-29 16:15:07 +0330

    feat: add mobile number && national code to users table

---
+++ database/migrations/2024_04_29_124022_add_mobile_number_and_national_code_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('mobile_number', 15)->nullable();
+            $table->char('national_code', 10)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('mobile_number');
+            $table->dropColumn('national_code');
+        });
+    }
+};
