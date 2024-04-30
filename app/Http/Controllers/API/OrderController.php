0403ed10c06663e0ef91492e4bddb4f0b36d9803
Mahdi Amereh <mahdieightt@gmail.com>
2024-04-30 09:16:20 +0330

    feat: update method index in OrderController

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers\API;
 
 use App\Contracts\Controller\API\OrderControllerInterface;
+use App\Facades\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\OrderIndexRequest;
+use App\Http\Resources\OrderCollection;
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class OrderController extends Controller implements OrderControllerInterface
 {
     public function index(OrderIndexRequest $request)
     {
+        $status = $request->validated('status');
+        $min_amount = $request->validated('amount.min');
+        $max_amount = $request->validated('amount.max');
+        $mobile_number = $request->validated('mobile_number');
+        $national_code = $request->validated('national_code');
+
+        $orders = Order::when($status, function (Builder $query) use ($status) {
+            $query->whereStatus($status);
+        })
+            ->when(($national_code || $mobile_number), function (Builder $query) use ($national_code, $mobile_number) {
+                $query->whereHas('user', function (Builder $userQuery) use ($national_code, $mobile_number) {
+
+                    $userQuery->when($national_code,  function (Builder $userQuery) use ($national_code) {
+                        $userQuery->where('national_code', 'LIKE', "%$national_code%");
+                    });
+
+                    $userQuery->when($mobile_number,  function (Builder $userQuery) use ($mobile_number) {
+                        $userQuery->where('mobile_number', 'LIKE', "%$mobile_number%");
+                    });
+                });
+            })
+            ->when(!is_null($min_amount), function (Builder $query) use ($min_amount) {
+                $query->where('amount', '>=', $min_amount);
+            })
+            ->when(!is_null($max_amount), function (Builder $query) use ($max_amount) {
+                $query->where('amount', '<=', $max_amount);
+            });
+            
+        return Response::message('order.messages.list_of_orders_has_been_received_successfully')->data(new OrderCollection($orders->paginate()))->status(201)->send();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'orders'], function () {
+    Route::get('/', [OrderController::class, 'index']);
+});
