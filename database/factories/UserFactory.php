24f8cc1716fa80370155081990152ef59c9319cb
Mahdi Amereh <mahdieightt@gmail.com>
2024-04-30 08:58:01 +0330

    feat: update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'mobile_number' => fake()->numerify('09#########'),
+            'national_code' => fake()->numerify('##########'),
+
         ];
     }
 
