f84e60ae4cc115e9a4c58d7e982275e33a0ddb20
Mahdi Amereh <mahdieightt@gmail.com>
2024-04-30 09:04:29 +0330

    feat: publish laravel lang

---
+++ lang/en/order.php
@@ -0,0 +1,18 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Order Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'enums' => [],
+    'messages' => [
+        'list_of_orders_has_been_received_successfully' => 'The list of orders has been received successfully',
+    ],
+    'validations' => [],
+    'errors' => [],
+];
